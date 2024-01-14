42c137a53ab3e13b24631e39736a4b2fd1849f89
mins 199600 <153487560+mins199600@users.noreply.github.com>
2024-01-14 19:00:20 +0900

    Add files via upload
    java practice 수행

---
+++ 01_JAVA_practice/chap01-literal-and-variable-practice/src/com/ohgiraffers/practice/Application2.java
@@ -0,0 +1,26 @@
+package com.ohgiraffers.practice;
+
+public class Application2 {
+
+    public static void main(String[] args) {
+
+        /* 사각형의 넓이와 둘레를 구하여 출력합니다.
+         * 너비 12.5 높이 36.4를 변수에 저장하고,
+         * 넓이와 둘레를 계산한 뒤 변수에 담아 아래와 같이 출력하세요.
+         *
+         * -- 출력 예시 --
+         * 면적 : 455.0
+         * 둘레 : 97.8
+         *  */
+
+        double width = 12.5;
+        double height = 36.4;
+
+       double extent = (width * height);
+       double diameter = 2 * (width + height);
+
+        System.out.println(extent);
+        System.out.println(diameter);
+    }
+
+}
